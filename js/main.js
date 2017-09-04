a7ce5746d2d61638ae6f1a7a96dcf0ea66b24bb8
大鹏 <yfzs_dapeng@163.com>
2017-09-04 12:22:39 +0800

    “修改部分样式

--- js/main.js
+++ js/main.js
@@ -10,9 +10,9 @@ $(function () {
 // banner
 $(function () {
     $(".banner").mouseenter(function () {
-        $(".banner_text").fadeToggle(3000);
+        $(".banner_text").fadeIn(2000);
     }).mouseleave(function () {
-        $(".banner_text").fadeToggle(3000);
+        $(".banner_text"). fadeOut(2000);
     })
 });
 
@@ -34,22 +34,22 @@ $(function () {
 });
 
 // 公司新闻轮播图
-$('.accordion>li').on('mouseenter', function () {
-    _this = $(this);
-    $(this).siblings().stop().animate({'width':288,'opacity':0.3},800).end().stop().animate({'width':864,'opacity':1},800);
-    $('.accordion img').removeClass('accordion_active');
-    $(this).find('img').addClass('accordion_active');
-    $('.accordion img').each(function (index,ele) {
-        if($(ele).hasClass('accordion_active')){
-            $(this).stop().animate({'left':-0,'zIndex':1},800);
-            $(this).parent().next().stop().animate({'paddingLeft':100});
-        }else{
-            $(this).stop().animate({'left':-288,'zIndex':0},800);
-            $(this).parent().next().stop().animate({'paddingLeft':30});
-        }
-    })
-})
-$('.accordion>li').eq(1).css('width',864).triggerHandler("mouseenter");
+// $('.accordion>li').on('mouseenter', function () {
+//     _this = $(this);
+//     $(this).siblings().stop().animate({'width':288,'opacity':0.3},800).end().stop().animate({'width':864,'opacity':1},800);
+//     $('.accordion img').removeClass('accordion_active');
+//     $(this).find('img').addClass('accordion_active');
+//     $('.accordion img').each(function (index,ele) {
+//         if($(ele).hasClass('accordion_active')){
+//             $(this).stop().animate({'left':-0,'zIndex':1},800);
+//             $(this).parent().next().stop().animate({'paddingLeft':100});
+//         }else{
+//             $(this).stop().animate({'left':-288,'zIndex':0},800);
+//             $(this).parent().next().stop().animate({'paddingLeft':30});
+//         }
+//     })
+// })
+// $('.accordion>li').eq(1).css('width',864).triggerHandler("mouseenter");
 
 // 常见问题
 $(function () {
